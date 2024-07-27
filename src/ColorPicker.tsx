b8df9fde6b3fc306a7548ecfda4d9c92419f5c1f
msinancoskun <sinancoskun2345@gmail.com>
2024-07-27 15:43:59 +0300

    ColorPicker component

---
+++ src/ColorPicker.tsx
@@ -0,0 +1,26 @@
+import React, { ChangeEvent, useState } from "react";
+
+function ColorPicker() {
+  const [color, setColor] = useState("#FFFFFF");
+
+  function handleColorChange(event: ChangeEvent<HTMLInputElement>) {
+    setColor(event.target.value);
+  }
+
+  return (
+    <div className="color-picker-container">
+      <h1>Color Picker</h1>
+      <div className="color-display" style={{ backgroundColor: color }}>
+        <p>Selected color: {color}</p>
+      </div>
+      <label>Select a Color:</label>
+      <input
+        type="color"
+        value={color}
+        onChange={(e) => handleColorChange(e)}
+      />
+    </div>
+  );
+}
+
+export default ColorPicker;
